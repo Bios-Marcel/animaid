61a269d57945f70704c72cbbb7e0131b4cc15b09
Marcel Schramm <marceloschr@googlemail.com>
2018-11-29 22:52:41 +0100

    Initial code for datastructure and some methods

---
+++ animate/animate.go
@@ -0,0 +1,41 @@
+package animate
+
+type Anime struct {
+	episodes    map[int]Episode
+	genres      []string
+	name        string
+	description string
+}
+
+type ReleaseState int
+
+const (
+	UNRELEASED ReleaseState = iota
+	AIRING
+	FINISHED
+	CANCELED
+)
+
+func (anime *Anime) GetAmountOfEpisodes() int {
+	return length(anime.episodes)
+}
+
+func (anime *Anime) GetAmountOfReleasedEpisodes() int {
+	//TODO
+	return 0
+}
+
+func (anime *Anime) GetAmountOfWatchedEpisodes() int {
+	//TODO
+	return 0
+}
+
+func (anime *Anime) GetReleaseState() ReleaseState {
+	//TODO
+	return UNRELEASED
+}
+
+type Episdode struct {
+	released bool
+	watched  bool
+}
---
+++ animate/go.mod
@@ -0,0 +1,1 @@
+module git.sr.ht/~biosmarcel/animate/animate
